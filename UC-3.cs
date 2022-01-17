eb31c5bc1754d2daf532b1807c03ca68c28156b6
Pranali <pranalilambat00@gmail.com>
2022-01-17 21:18:15 +0530

    QUE3

---
+++ UC-3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+    class UC_3
+    {
+        public void AddPartTime()
+        {
+            //constants
+            int PartTime = 2;
+            int IS_FULL_TIME = 1;
+            int RatePerHr = 20;
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            //computation
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else if (empCheck == PartTime)
+            {
+                empHrs = 4;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * RatePerHr;
+            Console.WriteLine("Daily Emp Wage : " + empWage);
+        }
+    }
+}
+    
