1cc3ab40cb7d314e8c07e6ba4145ccd08dfbdc5e
Pranali <pranalilambat00@gmail.com>
2022-01-17 21:50:13 +0530

    QUE14

---
+++ IComputeEmpWage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+   public interface IComputeEmpWage
+    {
+
+        public void addCompanyEmpWage(string company, int empRatePerhr, int numOfWorkingDays, int maxHrInMonth);
+        public void computeEmpWage();
+        public int getTotalWage(string company);
+    }
+}
