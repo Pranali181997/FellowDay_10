89c8c104c36739fc3ebeb6429994e74f1c2f436c
Pranali <pranalilambat00@gmail.com>
2022-01-17 08:38:22 +0530

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+            //UC_1 present = new UC_1();
+            //present.PresentOrAbsent();
+
+            //UC_2 dailywage = new UC_2();
+            //dailywage.DailyWages();
+
+            //UC_3 parttime = new UC_3();
+            //parttime.AddPartTime();
+
+            //UC_4 obj = new UC_4();
+            //obj.SwitchCase();
+
+            //UC_5 obj1 = new UC_5();
+            //obj1.WageForMonth();
+
+            //UC_6 obj2 = new UC_6();
+            //obj2.MaxDays();
+
+            //computeWages( );
+
+            //ComputeEmpWagesclass computeemp = new ComputeEmpWagesclass();
+            //computeemp.ComputeEmpWage("Dmart ",20,2,1);
+
+
+            //EmpWageBuilderObject empWageBuilderObject = new EmpWageBuilderObject("Dmart ",100,4,12);
+            //empWageBuilderObject.computeEmpWage();
+            //Console.WriteLine(empWageBuilderObject.tostring());
+
+            //EmpWageBuilderObject1.EmpWageBuilderArray empWageBuilder = new EmpWageBuilderObject1.EmpWageBuilderArray();
+            //empWageBuilder.addcompanyEmpWage("Dmart ",20,1,50);
+            //empWageBuilder.addcompanyEmpWage("Rel ",50, 10, 110);
+            //empWageBuilder.ComputeEmpWage();
+
+            //EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            //empWageBuilder.addCompanyEmpWage("Dmart " , 20, 2, 10);
+            //empWageBuilder.addCompanyEmpWage("Rel " , 10, 4, 30);
+            //empWageBuilder.computeEmpWage();
+            //Console.WriteLine("Total wage for Dmart Company: " + empWageBuilder.getTotalWage("Dmart"));
+
+
+        }
+
+        
+    }
+}
