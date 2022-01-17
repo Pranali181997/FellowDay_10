a63738307b6b9f8bb897be03ab1b43f95feed861
Pranali <pranalilambat00@gmail.com>
2022-01-17 08:39:20 +0530

    UC-1.cs

---
+++ UC-1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+    class UC_1
+    {
+        public void PresentOrAbsent()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Present");
+            }
+        }
+    }
+}
