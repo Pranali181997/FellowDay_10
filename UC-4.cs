62ce3beda77c23f17f43feba4c7a5c7e3b555e42
Pranali <pranalilambat00@gmail.com>
2022-01-17 21:18:53 +0530

    QUE4

---
+++ UC-4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+    class UC_4
+    {
+         //constants
+        public const int PartTime = 2;
+        public const int IS_FULL_TIME = 1;
+        public const int RatePerHr = 20;
+        public void SwitchCase()
+        {
+            
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            //computation
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case PartTime:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+                    empWage = empHrs * RatePerHr;
+            Console.WriteLine("Daily Emp Wage : " + empWage);
+        }
+    }
+}
