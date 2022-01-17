797af713ad7d9666355d1b97ecd8b39dc9522e77
Pranali <pranalilambat00@gmail.com>
2022-01-17 21:49:32 +0530

    QUE14

---
+++ EmployeeWageClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+    interface EmployeeWageClass
+    {
+        public int ComputeEmpWage(string company, int empRatePerhr, int numOfWorkingDays, int maxHrInMonth);
+
+    }
+}
